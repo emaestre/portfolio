49e46a7c6f199b4b91a226a49b8655a3fa95058e
Eleazar Maestre <eleazarenrique23@gmail.com>
2020-03-23 00:08:00 -0400

    adding the new to component to the project

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import Thumbnail from '../thumbnail';
  
 function Projects(props) {
   return (
     <div>
       <h1>Projects</h1>
+      <Thumbnail
+        link="/twitter"
+        image="http://twitter-image-url.jpg"
+        title="Twitter Newsfeed"
+      />
     </div>
   )
 }
