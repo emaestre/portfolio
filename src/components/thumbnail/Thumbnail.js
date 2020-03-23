85754b1a8db8dd3a1f56ab5384ff27eb489ae301
Eleazar Maestre <eleazarenrique23@gmail.com>
2020-03-23 00:07:28 -0400

    creating new thumbnail component

---
+++ src/components/thumbnail/Thumbnail.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+ 
+function Thumbnail(props) {
+    const {
+        link,
+        image,
+        title
+    } = props;
+
+  return (
+    <div className="project">
+        <Link to={link}>
+            <div className="project-image">
+                <img src={image} alt="Project"/>
+            </div>
+            <div className="project-title">{title}</div>
+        </Link>
+    </div>
+  )
+}
+ 
+export default Thumbnail;
