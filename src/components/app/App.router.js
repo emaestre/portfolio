38e6d44bafe54904cb4c724938df8980af8d81b6
Eleazar Maestre <eleazarenrique23@gmail.com>
2020-03-25 19:53:54 -0400

    Adding switch element on the router

--- src/components/app/App.router.js
+++ src/components/app/App.router.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Route } from 'react-router';
+import { Route, Switch } from 'react-router';
 import About from '../about';
 import Home from '../home';
 import Projects from '../projects';
 
 function AppRouter() {
     return (
-        <div>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/projects" component={Projects} />
-            <Route path="/about" component={About} />
-        </div>
+        <Switch>
+            <Route exact path="/portfolio" component={Home} />
+            <Route exact path="/portfolio/projects" component={Projects} />
+            <Route path="/portfolio/about" component={About} />
+        </Switch>
     );
 }
 
