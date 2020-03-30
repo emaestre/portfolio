b8c015090d70e178a71cdb556eed702046d82609
Eleazar Maestre <eleazarenrique23@gmail.com>
2020-03-30 11:33:16 -0400

    Fixing classes and importing sass file

--- src/components/thumbnail/Thumbnail.js
+++ src/components/thumbnail/Thumbnail.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Thumbnail.scss';
 
 function Thumbnail(props) {
     const { link, image, title } = props;
 
     return (
-        <div className="project">
+        <div className="Thumbnail">
             <Link to={link}>
-                <div className="project-image">
-                    <img src={image} alt="Project" />
+                <div className="Thumbnail__imageContainer">
+                    <img
+                        className="Thumbnail__image"
+                        src={image}
+                        alt="Project"
+                    />
                 </div>
-                <div className="project-title">{title}</div>
+                <div className="Thumbnail__title">{title}</div>
             </Link>
         </div>
     );
