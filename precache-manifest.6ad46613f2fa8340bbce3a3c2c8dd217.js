bd4f8e3f0bee3987b6c875afe2a821a7ce89c164
Eleazar Maestre <eleazarenrique23@gmail.com>
2020-03-30 21:24:51 -0400

    Updates

---
+++ precache-manifest.6ad46613f2fa8340bbce3a3c2c8dd217.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7336d7e560465ddaa05bcb8605b74eef",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "89d6907bf36721363044",
+    "url": "/portfolio/static/css/2.4d66749c.chunk.css"
+  },
+  {
+    "revision": "28caeb52509e7bad300c",
+    "url": "/portfolio/static/css/main.0b23bbe5.chunk.css"
+  },
+  {
+    "revision": "89d6907bf36721363044",
+    "url": "/portfolio/static/js/2.4c5d8f7b.chunk.js"
+  },
+  {
+    "revision": "28caeb52509e7bad300c",
+    "url": "/portfolio/static/js/main.b30a784c.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "5b4f4a60a83c7e586f770d0ee3b161a5",
+    "url": "/portfolio/static/media/alaska-4714097_1280.5b4f4a60.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.7f14456730bee2b4439fd53013a39d03.js"
+  "/portfolio/precache-manifest.6ad46613f2fa8340bbce3a3c2c8dd217.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
