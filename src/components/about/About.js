8deec3c2eb9eea6a229f6e0f071d05fb5247b997
Eleazar Maestre <eleazarenrique23@gmail.com>
2020-04-04 22:56:32 -0400

    test: Adding dummy data in the containers

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -4,6 +4,7 @@ function About(props) {
     return (
         <div>
             <h1>About</h1>
+            <p>This is the About component!</p>
         </div>
     );
 }
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,6 +4,7 @@ function Home(props) {
     return (
         <div>
             <h1>Home</h1>
+            <p>This is the Home component!</p>
         </div>
     );
 }
