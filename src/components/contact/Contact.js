26e826e010a1d098fb58bc54961d0d6ee2d47ee3
Eleazar Maestre <eleazarenrique23@gmail.com>
2020-05-19 08:03:45 -0400

    Feat: Adding initial contact form

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { Typography, Form, Input, Button } from 'antd';
+
+const { Paragraph, Title } = Typography;
 
 function Contact(props) {
     return (
         <div>
-            <h1>Contact</h1>
-            <p>This is the Contact component!</p>
+            <Title level={3}>Get in touch</Title>
+            <Paragraph>
+                I looking forward to hear from you! Just fill the format above
+                or contact me using my social networks.
+            </Paragraph>
+            <div>
+                <Form>
+                    <Form.Item label="Name">
+                        <Input placeholder="input placeholder" />
+                    </Form.Item>
+                    <Form.Item label="Subject">
+                        <Input placeholder="input placeholder" />
+                    </Form.Item>
+                    <Form.Item label="Message">
+                        <Input placeholder="input placeholder" />
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary">Send Message</Button>
+                    </Form.Item>
+                </Form>
+            </div>
         </div>
     );
 }
